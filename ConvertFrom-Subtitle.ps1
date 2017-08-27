b033cb0b8b5f7e80641b1375b8a470140a6b522d
Björn Sundling <bjorn.sundling@gmail.com>
2017-08-27 23:14:05 +0200

    ConvertFrom-Subtitle, initial commit

---
+++ ConvertFrom-Subtitle.ps1
@@ -0,0 +1,60 @@
+﻿function ConvertFrom-Subtitle
+{
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $InputObject
+    )
+
+    Begin
+    {
+        [int]$Length        = $InputObject.Length - 1
+        [int]$ReadLentgh    = 0
+        [TimeSpan]$TimeSpan = New-TimeSpan
+
+    }
+    Process
+    {
+        do
+        {
+            [int]$ReadLine      = $ReadLentgh
+            [int]$TimeLine      = $ReadLentgh + 1
+            [int]$TextStartLine = $ReadLentgh + 2
+            
+            $x = $TextStartLine
+            DO {
+                $x++
+            } 
+            While (
+                $InputObject[$x] -ne ''
+            ) 
+            [int]$TextEndLine   = $x - 1
+                             
+
+            $startTime = $timespan.Add( (($InputObject[($TimeLine)] -replace ',','.') -split ' ' )[0] )
+            $EndTime   = $timespan.Add( (($InputObject[($TimeLine)] -replace ',','.') -split ' ' )[2] )
+            $Duration  = $EndTime - $StartTime
+            $Text      = $inputObject[$TextStartLine..$textEndLine]
+
+            [array]$Output += New-Object -TypeName psobject -Property @{
+                'Line'      = [int]$InputObject[$ReadLine]
+                'StartTime' = [timespan]$startTime
+                'EndTime'   = [timespan]$EndTime
+                'Duration'  = [timespan]$Duration
+                'text'      = [string[]]$Text
+            }
+
+            $ReadLentgh = $x + 1
+        }
+        until ($ReadLentgh -ge $Length)
+        
+    }
+    End
+    {
+        $Output
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SubtextFunctions
 Functions for importing and exporting subtext files (.srt)
+
+Still very Alpha.
+
+Mostly a project to get myself to try out classes,
+
+but if someone finds it usefull, go ahead.
+
+Todo
+* nicer way to find text start and stopline in ConvertFrom-Subtitle
+* Create class(es) for subtitles
+    * $SubObject.AddSeconds(5) # should move entire subtitle five seconds forward.
+* ConvertTo-subtitle
+* Pester tests 
+* Add support for other formats, WebVTT and TTML maybe?
